58c8754cfed0ceffa5a3ec1fea741af5079aaed4
OzZySanzzz <165535487+lrzozz@users.noreply.github.com>
2024-07-12 10:31:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 source $FUNCTION
 import axeron.prop
 local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
-local vip="https://lrzozz.github.io/MCBoost/id_vip.txt"
+local vip="https://lrzozz.github.io/LunarBoost/vip/install.sh"
 local id="$(settings get secure android_id)"
-local check_id="$(storm)"
+local check_id="$(storm https://lrzozz.github.io/LunarBoost/id_vip.txt)"
 local trim_id="${id:0:6}"
 local vip_version=$(echo "$check_id" | grep -q "$trim_id" && echo true || echo false)
 if [ $vip_version = true ]; then
